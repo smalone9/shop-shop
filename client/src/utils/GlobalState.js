6aed1cac2b51dfda3fb9efd6454aa42805f58fb2
Samantha Malone <sammac99@gmail.com>
2021-09-21 08:53:13 -0700

    connected global state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -3,3 +3,20 @@ import { useProductReducer } from "./reducers";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: "",
+  });
+  // use this to confirm it works!
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
