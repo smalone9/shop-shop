df1ba0b4d907a65e5fb842d469ef25b644c981e9
Samantha Malone <sammac99@gmail.com>
2021-09-23 23:13:27 -0700

    added shopping cart layout

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
+import Cart from "../components/Cart";
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from "../assets/spinner.gif";
@@ -51,6 +52,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
